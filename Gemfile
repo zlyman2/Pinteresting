cf91d125a039bcae6d39d5ccee1216fccc451e60
Zachary Lyman <zachlyman2@gmail.com>
2014-12-04 12:20:56 -0500

    implemented devise for users

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-ruby '2.0.0'
+ruby '2.1.5'
 
 
 
@@ -20,6 +20,8 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0'
 
 gem 'bootstrap-sass'
+
+gem 'devise',"~> 3.1.0.rc2"
 
 group :development, :test do
 	gem 'sqlite3'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # note to set this to actual host name
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
